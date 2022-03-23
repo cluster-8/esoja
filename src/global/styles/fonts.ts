af74246f721731ae67bfad4f8d012d2706525985
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-03-23 12:33:19 -0300

    create mobile branch

--- src/global/styles/fonts.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  fonts: {
-    regular: 'Poppins_400Regular',
-    medium: 'Poppins_500Medium',
-    semiBold: 'Poppins_600SemiBold',
-    bold: 'Poppins_700Bold',
-  },
-};
--- src/global/styles/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-import theme from './theme';
-
-declare module 'styled-components' {
-  type ThemeType = typeof theme.dark;
-
-  export interface DefaultTheme extends ThemeType {}
-}
