3b43af8c34acab8c0e2d7fe846ce05deebc2b3d0
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-03-30 07:56:18 -0300

    create front endfolder

---
+++ frontend/src/screens/Weather/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+import { SignInScreenRouteProps } from "../../data/routes/auth";
+
+export const SignIn: React.FC<SignInScreenRouteProps> = ({ navigation }) => {
+  return (
+    <View>
+      <Text>will</Text>
+    </View>
+  );
+};
