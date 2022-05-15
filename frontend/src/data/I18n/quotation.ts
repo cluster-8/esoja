13c50a5eaffceb97035955cdbac2c407c527b64e
vinicius-hso <viniciushso@icloud.com>
2022-05-15 18:47:29 -0300

    Add Quotation Translate

--- frontend/src/data/I18n/quotation.ts
+++ frontend/src/data/I18n/quotation.ts
@@ -1,16 +1,30 @@
 export default {
   pt: {
     quotation: {
-      title: 'Cotações',
-      subtitle:
-        'Selecione uma cidade e fique por dentro das últimas atualizaçôes do custo da soja'
+      seedPageTitle: 'Cotações das sementes',
+      bagPageTitle: 'Cotações das sacas',
+      pageDescription:
+        'Selecione uma cidade e fique por dentro das últimas atualizaçôes do custo da soja',
+      selectTitle: 'Selecione a cidade',
+      date: 'Data',
+      bagPrice: 'Preço sc 60kg',
+      seedPrice: 'Preço sementes',
+      variation: 'Variação',
+      chartTitle: 'Variação do preço da soja R$ (sc 60kg)'
     }
   },
   en: {
     quotation: {
-      title: 'Quotation',
-      subtitle:
-        'Select a city and stay up to date with the latest soy cost updates'
+      seedPageTitle: 'Seeds quotations',
+      bagPageTitle: 'Bags quotations',
+      pageDescription:
+        'Select a city and stay up to date with the latest soy cost updates',
+      selectTitle: 'Select city',
+      date: 'Date',
+      bagPrice: 'Price Bag 60kg',
+      seedPrice: 'Price seeds',
+      variation: 'Variation',
+      chartTitle: 'Soybean (bag 60kg) price variation'
     }
   }
 };
