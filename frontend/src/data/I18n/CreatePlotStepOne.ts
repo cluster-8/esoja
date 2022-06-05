a805726a8021745f07e2119af8769ff43fc0d76e
Elias Ferreira <elias.faria@fatec.sp.gov.br>
2022-06-05 12:20:58 -0300

    fixed conflits on i18n

--- frontend/src/data/I18n/CreatePlotStepOne.ts
+++ frontend/src/data/I18n/CreatePlotStepOne.ts
@@ -2,29 +2,6 @@ export default {
   pt: {
     CreatePlotStepOne: {
       title: 'Coordenadas do talhão',
-<<<<<<< HEAD
-      subtitle: 'Marque no mapa a localização e area correspondente ao talhão',
-      questionTitle: 'Como deseja prosseguir?',
-      buttonTitlePolygon: 'Desenhar talhão no mapa',
-      buttonMessagePolygon: 'marque todos os pontos do talhão e calcularemos a área automaticamente',
-      buttonTitlePoint: 'Marcar ponto único',
-      buttonMessagePoint: 'marque o ponto central do talhão e a area manualmente',
-      ContinueButton: 'Continuar',
-      ModeText: 'Alterar modo',
-      areaLabel: 'Area',
-      areaPlaceholder: 'Area do talhão em ha',
-      errors: {
-        polygonAlert: {
-          localFormat: 'Localização inválida',
-          map: 'Marque o talhão no mapa',
-          min: 'Marque no mínimo 3 pontos ou escolha o modo ponto único',
-          fieldFormat: 'Talhão inválido'
-        },
-        areaTotal: {
-          required: 'Quantidade é obrigatória'
-        }
-      }
-=======
       subtitle: 'Desenhe no mapa a área correspondente do talhão',
       ClearButton: 'Limpar',
       ContinueButton: 'Continuar',
@@ -37,35 +14,10 @@ export default {
       markPointButtonMsg:
         'Marque o ponto central do tralhao e informe a area manualmente',
       modeText: 'Alterar modo'
->>>>>>> 148aa8128eedf2515ed243156ec3cf5200915182
     }
   },
   en: {
     CreatePlotStepOne: {
-<<<<<<< HEAD
-      title: 'Field coordinates',
-      subtitle: 'Mark on the map the location and area corresponding to the field',
-      questionTitle: 'How do you want to proceed?',
-      buttonTitlePolygon: 'Draw field on map',
-      buttonMessagePolygon: 'mark all points in the field and we will calculate the area automatically',
-      buttonTitlePoint: 'Mark single point',
-      buttonMessagePoint: 'mark field midpoint and area manually',
-      ContinueButton: 'Continue',
-      ModeText: 'Change mode',
-      areaLabel: 'Area',
-      areaPlaceholder: 'Field area in ha',
-      errors: {
-        polygonAlert: {
-          localFormat: 'Invalid location',
-          map: 'Mark field on map',
-          min: 'Score at least 3 points or choose single point mode',
-          fieldFormat: 'Invalid field'
-        },
-        areaTotal: {
-          required: 'Quantity is required'
-        }
-      }
-=======
       title: 'Plots coordinates',
       subtitle: 'Draw on the map the corresponding area of the field',
       ClearButton: 'Clear',
@@ -79,7 +31,6 @@ export default {
       markPointButtonMsg:
         'Mark the center point of the work and enter the area manually',
       modeText: 'Change mode'
->>>>>>> 148aa8128eedf2515ed243156ec3cf5200915182
     }
   }
 };
--- frontend/src/data/I18n/CreatePlotStepTwo.ts
+++ frontend/src/data/I18n/CreatePlotStepTwo.ts
@@ -3,19 +3,6 @@ export default {
     CreatePlotStepTwo: {
       title: 'Identifique o talhão',
       subtitle: 'Insira um nome e uma descrição para o seu novo talhão',
-<<<<<<< HEAD
-      defaultValueLabel: 'Selecione uma propriedade',
-      fieldName: 'Nome',
-      fieldNamePlaceholder: 'Digite um nome para o talhão',
-      plantingDateLabel: 'Data de plantio',
-      plantingDatePlaceholder: 'Data do plantio',
-      cropYearLabel: 'Ano safra',
-      cropYearPlaceholder: 'Informe o ano safra',
-      fieldDescription: 'Descrição',
-      fieldDescriptionPlaceholder: 'Digite uma descrição',
-      continueButton: 'Finalizar',
-      genderLabel: 'Selecione a propriedade',
-=======
 
       fieldPropertyLabel: 'Selecione e propriedade do talhão',
 
@@ -32,7 +19,6 @@ export default {
       fieldDescriptionPlaceholder: 'Digite uma descrição',
 
       continueButton: 'Continuar',
->>>>>>> 148aa8128eedf2515ed243156ec3cf5200915182
       errors: {
         stepTwoName: {
           required: 'Nome é obrigatório'
@@ -53,15 +39,6 @@ export default {
     CreatePlotStepTwo: {
       title: 'Identify the field',
       subtitle: 'Enter a name and description for your new field',
-<<<<<<< HEAD
-      defaultValueLabel: 'Select a property',
-      fieldName: 'Name',
-      fieldNamePlaceholder: 'Enter a name for the field',
-      plantingDateLabel: 'Planting date',
-      plantingDatePlaceholder: 'Planting Date',
-      cropYearLabel: 'Year crop',
-      cropYearPlaceholder: 'Enter the crop year',
-=======
 
       fieldPropertyLabel: "Select  property's field",
 
@@ -74,7 +51,6 @@ export default {
       fieldCropYear: 'Crop year',
       fieldCropYearPlaceholder: 'Enter crop year',
 
->>>>>>> 148aa8128eedf2515ed243156ec3cf5200915182
       fieldDescription: 'Description',
       fieldDescriptionPlaceholder: 'Enter a description',
       continueButton: 'Finish',
