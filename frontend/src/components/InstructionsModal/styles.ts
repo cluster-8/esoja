515947265ced8243237f8254976cfff7d3ecbc72
carolinam <carolinamargiott2i@gmail.com>
2022-09-29 11:50:31 -0300

    Arrumado estilo para o modal aparecer em tela cheia

--- frontend/src/components/InstructionsModal/styles.ts
+++ frontend/src/components/InstructionsModal/styles.ts
@@ -1,42 +1,22 @@
 import { Feather } from '@expo/vector-icons';
-import { FlatList, FlatListProps } from 'react-native';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
-import { Property } from '../../data/Model/Property';
 import { RFFontSize, RFHeight, RFWidth } from '../../utils/getResponsiveSizes';
 
-export const LocationList = styled(
-  FlatList as new (props: FlatListProps<Property>) => FlatList<Property>
-).attrs({
-  showsVerticalScrollIndicator: false,
-  contentContainerStyle: {
-    paddingBottom: getBottomSpace(),
-    display: 'flex',
-    justifyContent: 'space-between'
-  }
-})``;
-
-export const CenteredView = styled.View`
+export const FullView = styled.View`
   flex: 1;
-  justify-content: flex-end;
   align-items: center;
-  margin-top: ${RFHeight(22)}px;
-  background-color: transparent;
-  margin-top: auto;
+  margin-top:0px;
+  background-color: ${({ theme }) => theme.colors.background};
+    border-top-left-radius: ${RFHeight(32)}px;
+  border-top-right-radius: ${RFHeight(32)}px;
+
 `;
 
 export const ModalStyled = styled.Modal`
-  /* background-color: ${({ theme }) => theme.colors.background}; */
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const ModalView = styled.View`
-  position: relative;
-  border-top-left-radius: ${RFHeight(32)}px;
-  border-top-right-radius: ${RFHeight(32)}px;
-  width: 100%;
-  overflow: hidden;
-  height: 30%;
-  background-color: ${({ theme }) => theme.colors.background_over};
   padding: ${RFHeight(16)}px;
 `;
 
@@ -62,19 +42,4 @@ export const TextStyled = styled.Text`
 export const ModalText = styled.Text`
   margin-bottom: ${RFHeight(15)}px;
   text-align: center;
-`;
-
-export const CloseButtonIcon = styled(Feather)`
-  font-size: ${RFFontSize(24)}px;
-  color: ${({ theme }) => theme.colors.attention};
-`;
-
-export const CloseButton = styled.TouchableOpacity`
-  position: absolute;
-  right: ${RFWidth(16)}px;
-  top: ${RFHeight(16)}px;
-  height: ${RFHeight(30)}px;
-  width: ${RFWidth(30)}px;
-  align-items: center;
-  justify-content: center;
 `;
