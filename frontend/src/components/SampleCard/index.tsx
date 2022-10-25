b6b4fc7bdcf0f5a600be306a947a54fa4f8bc761
carolinam <carolinamargiott2i@gmail.com>
2022-10-25 09:42:45 -0300

    adicionado botão na pagina

--- frontend/src/components/SampleCard/index.tsx
+++ frontend/src/components/SampleCard/index.tsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Sample } from '../../data/Model/Sample';
 import { SampleData, SampleInformationContainer, SampleName } from './styles';
 import { translate } from '../../data/I18n';
+import { Alert, Button } from 'react-native';
+import { useSample } from '../../hooks/useSample';
 
 interface SampleCardProps {
   sample: Sample;
+  teste:any;
 }
 
-export const SampleCard: React.FC<SampleCardProps> = ({ sample }) => {
+export const SampleCard: React.FC<SampleCardProps> = ({ sample,teste }) => {
+  const {deleteSample} = useSample();
+
+    const removeSample = useCallback(async ()=>{
+    try {
+      await deleteSample(sample.id);
+      teste();
+    } catch (error) {
+      Alert.alert(
+            'Erro',
+            'Não foi possível deletar amostra'
+          );
+    }
+    },[sample])
+
   return (
     <SampleInformationContainer>
-      <SampleName>{sample?.name}</SampleName>
-      <SampleData>
-        {translate('plots.PlotCardPlants.plant1')}: {sample?.grainsPlant1}{' '}
-        {translate('plots.PlotCardPlants.grains')}
-      </SampleData>
-      <SampleData>
-        {translate('plots.PlotCardPlants.plant2')}: {sample?.grainsPlant2}{' '}
-        {translate('plots.PlotCardPlants.grains')}
-      </SampleData>
-      {!!sample?.description && (
+      <>
+        <SampleName>{sample?.name}</SampleName>
         <SampleData>
-          {translate('plots.PlotCardPlants.description')}: {sample?.description}
+          {translate('plots.PlotCardPlants.plant1')}: {sample?.grainsPlant1}{' '}
+          {translate('plots.PlotCardPlants.grains')}
         </SampleData>
-      )}
+        <SampleData>
+          {translate('plots.PlotCardPlants.plant2')}: {sample?.grainsPlant2}{' '}
+          {translate('plots.PlotCardPlants.grains')}
+        </SampleData>
+        {!!sample?.description && (
+          <SampleData>
+            {translate('plots.PlotCardPlants.description')}: {sample?.description}
+          </SampleData>
+        )}
+      </>
+      <Button title='Deletar' onPress={()=>{removeSample()}} />
     </SampleInformationContainer>
   );
 };
